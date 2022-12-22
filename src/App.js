7170c164a54404db30e4aeda8bd4be3ac7241050
DRE-stac <81095893+DRE-stac@users.noreply.github.com>
2022-12-21 21:14:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -246,19 +246,6 @@ export const StyledImg = styled.img`
 `;
 
 
-
-  const widget = new Widget({
-    target: '',
-    appName: 'DrePay',
-    autoShowTime: 5,
-    position: 'center',
-    height: 700,
-    width: 600,
-    overlay: false,
-    network: 'mainnet',
-    closable: true,
-  });
-widget.create()
   function App() {
 
 
